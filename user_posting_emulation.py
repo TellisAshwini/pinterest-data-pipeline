94d066d9069f3a25db63276191a1c353e4a33371
TellisAshwini <102413647+TellisAshwini@users.noreply.github.com>
2023-05-04 19:53:06 +0100

    updates 'run_infinite_post_data_loop' function to send data to kafka topics in the user_posting_emulation file

--- user_posting_emulation.py
+++ user_posting_emulation.py
@@ -1,14 +1,15 @@
 import requests
 from time import sleep
-import random
+#import random
 from multiprocessing import Process
 import boto3
 import json
 import sqlalchemy
 from sqlalchemy import text
+import datetime
 
 
-random.seed(100)
+#random.seed(100)
 
 
 class AWSDBConnector:
@@ -22,38 +23,67 @@ class AWSDBConnector:
         self.PORT = 3306
         
     def create_db_connector(self):
-        engine = sqlalchemy.create_engine(f"mysql+pymysql://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DATABASE}?charset=utf8mb4")
+        engine = sqlalchemy.create_engine(f"mysql+pymysql://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DATABASE}?charset=utf8mb4", pool_pre_ping=True)
         return engine
 
 
 new_connector = AWSDBConnector()
+url = "https://mhmruluzt9.execute-api.us-east-1.amazonaws.com/prod/topics/"
+headers = {'Content-Type': 'application/vnd.kafka.json.v2+json'}
 
+def datetime_converter(date_time):
+    if isinstance(date_time, datetime.datetime):
+        return date_time.__str__()
 
 def run_infinite_post_data_loop():
     while True:
-        sleep(random.randrange(0, 2))
-        random_row = random.randint(0, 11000)
+        #sleep(random.randrange(0, 2))
+        #random_row = random.randint(0, 11000)
         engine = new_connector.create_db_connector()
-
         with engine.connect() as connection:
 
-            pin_string = text(f"SELECT * FROM pinterest_data LIMIT {random_row}, 1")
-            pin_selected_row = engine.execute(pin_string)
-            
+            pin_string = text(f"SELECT * FROM pinterest_data")
+            pin_selected_row = connection.execute(pin_string)
+            pin_list,geo_list, user_list = [], [], []
+            topic = "12f7a43505b1.pin"
+            invoke_url = url+topic
+            print('starts topic pin')
             for row in pin_selected_row:
                 pin_result = dict(row._mapping)
+                pin_list.append({"value":pin_result})
+            payload = json.dumps({"records": pin_list})
+            response = requests.request("POST", invoke_url, headers=headers, data=payload)
+            print(response.status_code)
 
-            geo_string = text(f"SELECT * FROM geolocation_data LIMIT {random_row}, 1")
-            geo_selected_row = engine.execute(geo_string)
+        
+            print('starts topic geo')
+            geo_string = text(f"SELECT * FROM geolocation_data")
+            geo_selected_row = connection.execute(geo_string)
             
+            topic = "12f7a43505b1.geo"
+            invoke_url = url+topic
             for row in geo_selected_row:
                 geo_result = dict(row._mapping)
-
-            user_string = text(f"SELECT * FROM user_data LIMIT {random_row}, 1")
-            user_selected_row = engine.execute(user_string)
+                geo_list.append({"value":geo_result})
+            payload = json.dumps({"records": geo_list}, default = datetime_converter)
+            response = requests.request("POST", invoke_url, headers=headers, data=payload)
+            print(response.status_code)
             
+           
+            print('starts topic user')
+            user_string = text(f"SELECT * FROM user_data")
+            user_selected_row = connection.execute(user_string)
+            
+            topic = "12f7a43505b1.user"
+            invoke_url = url+topic
             for row in user_selected_row:
                 user_result = dict(row._mapping)
+                user_list.append({"value":user_result})
+            payload = json.dumps({"records": user_list}, default = datetime_converter)
+            response = requests.request("POST", invoke_url, headers=headers, data=payload)
+            print(response.status_code)
+            print('Completed!')
+            break 
 
 
 if __name__ == "__main__":
